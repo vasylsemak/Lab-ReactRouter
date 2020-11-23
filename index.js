f2c352523a2410d88f7f27a23f21b9400a69ef3a
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-23 16:31:49 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Red = (props) => {
+  return (
+    <div className='red'>
+      <h1>RED</h1>
+    </div>
+  )
+}
+
+const Blue = (props) => {
+  return (
+    <div className='blue'>
+      <h1>BLUE</h1>
+    </div>
+  )
+}
+
+const Main = (props) => {
+  return (
+    <div id='container'>
+
+      <div id='navbar'>
+        {/* navigation here */}
+      </div>
+
+      <div id='main-section'>
+        {/* routes here */}
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'))
